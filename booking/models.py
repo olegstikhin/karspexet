1d225ed0e2126fc9b67cb151025be0e80becf9da
Oleg Stikhin <oleg.stikhin@gmail.com>
2015-12-15 05:19:46 +0200

    added correct views

--- booking/models.py
+++ booking/models.py
@@ -9,9 +9,15 @@ class Participant(models.Model):
     diet = models.CharField(max_length=128)
     alcoholfree = models.BooleanField()
     comment = models.TextField()
-    discount_code = models.ForeignKey('DiscountCode', db_column='participants')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class DiscountCode(models.Model):
     code = models.CharField(max_length=8)
     price = models.FloatField()
+
+    def __str__(self):
+        return self.code
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,25 @@
+from django import forms
+from django.core.validators import validate_email
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from booking.models import *
 
 # Create your views here.
 def form_page_view(request):
-    return render(request, "index.html")
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        try:
+            validate_email(email)
+        except forms.ValidationError:
+            return HttpResponse("<p>Din e-postadress är inte giltig, vänligen försök på nytt</p><p><a href='./'>Tillbaka</a></p>")
+        discount_code = request.POST['discount_code']
+        nachspex = request.POST.get('nachspex', False)
+        alcoholfree = request.POST.get('alcoholfree', False)
+        diet = request.POST['diet']
+        comment = request.POST['comment']
+        new_participant = Participant(name=name, email=email, nachspex=nachspex, alcoholfree=alcoholfree, diet=diet, comment=comment)
+        new_participant.save()
+        return render(request, "index.html")
+    else:
+        return render(request, "index.html")
